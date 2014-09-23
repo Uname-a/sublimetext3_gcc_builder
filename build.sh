b26f06764d7d934447d85b5228db98513cb23bda
matt baird <billybob2435@gmail.com>
2014-09-23 17:49:08 -0500

    Changes to be committed:  	modified:   build.sh     added file config maker
     	modified:   buildrun.sh
        added file config reader

--- build.sh
+++ build.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 cd $3
-
+echo $3
 arr=( $(find  -maxdepth 1 -type f  -name "*.c" ! -name "$2.*"  ))
 
 echo ${arr[@]}
 build=()
 echo "Other files to build with? enter the name without the './', then press enter:"
 read -a build 
-
+echo
 gcc -g -Wall $1 ${build[@]} -o $2
 
 touch .buildconfig
 printf "%s " "${build[@]}" > .buildconfig
 input=()
-echo "enter command line input leave blank if none, then press enter:"
+echo "enter argvsto be run with, leave blank if none, then press enter:"
 read -a input
+echo
 touch .buildargv
 printf "%s " "${input[@]}" > .buildargv
-
+echo "Running Program:"
+echo
 ./$2 ${input[@]}
 
 echo "
 
 ------------------
 (program exited with code: $?)"   
-
+echo
 
 read  -p "Hit ENTER to close"
--- buildrun.sh
+++ buildrun.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
 cd $3
-build=()
-readarray build < ./.buildconfig
-echo "Being built with:"
-echo ${build[@]}
+if [ -f  ./.buildconfig ]; then
+	build=()
+	read -a build < ./.buildconfig
+	echo "Being built with:"
+	echo ${build[@]}
+	echo
+	gcc -g -Wall $1 ${build[@]} -o $2
 
+else 
+	echo "Need to run 'build' at least once"
+	echo
+	read -p "Hit ENTER to close"
+	exit
+fi
 
-gcc -g -Wall $1 ${build[@]} -o $2
+if [ -f  ./.buildargv ]; then
+	input=()
+	read -a input < ./.buildargv
+	echo "Being run with argvs:"
+	echo ${input[@]}
+	echo
+	echo "Runnig program:"
+	echo
+	./$2 ${input[@]}
 
-input=()
-readarray input < ./.buildargv
-echo "Being run with:"
-echo ${input[@]}
-
-./$2 ${input[@]}
-
-echo "
+	echo "
 
 ------------------
 (program exited with code: $?)"   
 
-
-read  -p "Hit ENTER to close"
+	echo
+	read  -p "Hit ENTER to close"
+else 
+	echo "Need to run 'build' at least once"
+	echo
+	read -p "Hit ENTER to close"
+	exit
+fi
